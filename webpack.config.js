d77f4f4200b6ef2253811674490b6d5bec26be62
gevangen <gevangen@hotmail.fr>
2020-01-30 15:02:27 +0100

    etape6

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,23 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const webpackConfig = {
+  plugins: [
+      /**
+       * All files inside webpack's output.path directory will be removed once, but the
+       * directory itself will not be. If using webpack 4+'s default configuration,
+       * everything under <PROJECT_DIR>/dist/ will be removed.
+       * Use cleanOnceBeforeBuildPatterns to override this behavior.
+       *
+       * During rebuilds, all webpack assets that are not used anymore
+       * will be removed automatically.
+       *
+       * See `Options and Defaults` for information
+       */
+      new CleanWebpackPlugin(),
+  ],
+};
 
 module.exports = {
   module: {
@@ -25,6 +43,21 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "src/index.html"
-    })
-  ]
+    }),
+    /**
+     * All files inside webpack's output.path directory will be removed once, but the
+     * directory itself will not be. If using webpack 4+'s default configuration,
+     * everything under <PROJECT_DIR>/dist/ will be removed.
+     * Use cleanOnceBeforeBuildPatterns to override this behavior.
+     *
+     * During rebuilds, all webpack assets that are not used anymore
+     * will be removed automatically.
+     *
+     * See `Options and Defaults` for information
+     */
+    new CleanWebpackPlugin(),
+  ],
+  devServer: {
+    open: true
+  }
 };
