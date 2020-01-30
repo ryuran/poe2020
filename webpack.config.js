06f2bdde13aa1d09f5a4a94c6ad5ae41b7e5e4d7
Clement Franch <glukor@hotmail.fr>
2020-01-30 12:03:57 +0100

    webpack config index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
-const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index_bundle.js'
   },
+  plugins: [new HtmlWebpackPlugin()]
 };
+
+
