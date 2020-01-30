f655abadb33348a541f9986d3174aefe743860a5
Emmanuel Phirmis <meagamanu@gmail.com>
2020-01-30 14:29:52 +0100

    add sass-loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,21 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
@@ -11,7 +26,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: 'src/index.html'
     })
-
+  ]
 };
 
 
