ca762546442dd1cbc730a27067882696285a33df
vreymond <valentinreymond@gmail.com>
2020-01-30 16:27:21 +0100

    Etape 8 Masta Sensei \o/

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,8 @@ module.exports = {
           // Translates CSS into CommonJS
           "css-loader",
           // Compiles Sass to CSS
-          "sass-loader"
+          "sass-loader",
+          'postcss-loader'
         ]
       }
     ]
